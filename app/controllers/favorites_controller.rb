8c2c86452a0f33b9e5b8d07c28557c8bb9febdbf
R.Kawasaki <gettodowithallhaste9@gmail.com>
2020-04-06 08:06:08 +0000

    Fix Favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = 'お気に入りにしました。'
-    redirect_to root_url
+    redirect_to likes_user_path(current_user)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to root_url
+    redirect_to likes_user_path(current_user)
   end
 end
